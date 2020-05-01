e504fa68bf1905ba6d2c9bd9086f41dd4926a953
Muhammad Ali <59179161+080HACKER@users.noreply.github.com>
2020-05-01 11:33:48 +0100

    Add files via upload

---
+++ NAIJA/tools/Quigm.py
@@ -0,0 +1,23 @@
+import os,sys,time
+
+os.system('clear')
+
+def babi(nob):
+  for e in nob:
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+babi('ASSALAMU ALAIKUM MUHAMMAD ALI HERE AGAIN!')
+print
+babi('CORONOVIRUS WANT TO FINISH THE WORLD PEOPLES SO')
+print
+babi('DONT BE WECKED STAY AT HOME AND FOLLOW THE gv RULES')
+print
+babi('YOU WILL BE ASK FOR THE TOOL KEY SO YOU JUST HAVE TO DOWNLOAD KEY')
+print
+babi('THANKS FOR USING MY TOOL LOL.....')
+
+os.system('sh log2.sh')
+
+exit()
