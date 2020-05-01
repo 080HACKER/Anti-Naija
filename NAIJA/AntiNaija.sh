6a8712ee84e014972fa686e69bb763a7bc8f8c12
Muhammad Ali <59179161+080HACKER@users.noreply.github.com>
2020-05-01 00:55:20 -0700

    Add files via upload

---
+++ NAIJA/AntiNaija.sh
@@ -0,0 +1,70 @@
+clear
+a="\033[30;1m"
+m="\033[31;1m"
+h="\033[32;1m"
+k="\033[33;1m"
+b="\033[34;1m"
+c="\033[35;1m"
+pu="\033[36;1m"
+p="\033[37;1m"
+m1="\033[38;1m"
+p2="\033[39;1m"
+hi="\033[40;1m"
+bi='[34;1m' #biru
+i='[32;1m' #ijo
+pur='[35;1m' #purple
+cy='[36;1m' #cyan
+me='[31;1m' #merah
+pu='[37;1m' #putih
+ku='[33;1m' #kuning
+mer='[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='[0m'
+sleep 1
+echo "${h}     ${h}NIGERIAN ${p}LITE ${m}CYBERSECURITY TEAM"
+echo "${h}        WHATSAPP ${p}ME: +234${h}9035145696"
+echo "${k}  ╔══════════════════════════════════════╗"
+echo "${h}    nn  n ñ nnnn   nnnn n nn ñ nnnn nn  n       "
+echo "${p}    n n n n n  nnn nnnn nnnn n nnnn n n n       "
+echo "${h}    n  nn n nnnn n nnnn n  n n n  n n  nn       "
+echo "${k}  ╚══════════════════════════════════════╝ ${h}v6.9"
+echo "${h}      ${m}${p1}${h}[${p1}${p}CREATED BY${h}]${m}• >${h}[${p1}MUHAMMAD ${p}ALI ${h}NG${h}]${m}${p1}"
+echo "${k} <════════════════════════════════════════>"
+echo "${k}              ║ ${me}#STAY SAFE ${k}║"                                         
+echo $i"╔═══╗"$i"╔═════════════╗"
+echo $i"║$pu A$i ║"$i"║$pu LOGIN TOOL $i ║"
+echo $i"╚═══╝"$i"╚═════════════╝"
+echo $i"╔═══╗"$i"╔═══════════════════════╗"
+echo $i"║$pu B$i ║"$i"║$pu DOWNLOAD THE TOOL KEY$i ║"
+echo $i"╚═══╝"$i"╚═══════════════════════╝"
+echo $i"╔═══╗"$i"╔════════════════════════════╗"
+echo $i"║$pu C$i ║"$i"║$pu JOIN TO OUR NIGERIAN TEAM$i  ║"
+echo $i"╚═══╝"$i"╚════════════════════════════╝"
+echo
+echo $i"╔═══$me YOUR CHOICE?$i ════"
+read -p "╚═══════════════➢➢ " pil
+
+if [ $pil = 'A' ]
+then
+cd tools
+python2 Quigm.py
+fi
+if [ $pil = 'B' ]
+then
+echo $pu"OPEN YOUR BROWSER"
+sleep 2
+xdg-open http://gestyy.com/w9MG6x
+
+fi
+if [ $pil = 'C' ]
+then
+echo ${p}"THIS WILL AUTHOUMATICALL OPEN YOUR BROWSER TO JOIN THE TEAM"
+sleep 2
+xdg-open http://gestyy.com/w9Z6Df
+fi
